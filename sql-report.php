889097f9052dbffc70fc18ed8a3578bc992a9d7e
Kittiphak wannapanich <soulkyo888@gmail.com>
2016-10-31 05:08:25 +0700

    report asset ได้เเล้ว

--- sql-report.php
+++ sql-report.php
@@ -3,29 +3,118 @@
 if ($_POST) {
 
   include('connect.php');
-$startDate = $_POST['startDate'];
-$enddate = $_POST['enddate'];
+$startDate = $_POST['assetStartDate'];
+$enddate = $_POST['assetEndDate'];
+$assetType = $_POST['assetType'];
+$assetVendor = $_POST['assetVendor'];
+$assetstatus = $_POST['assetStatus'];
+
+//echo "assetStatus $assetstatus<br >";
+//echo "assetType $assetType<br >";
+if($assetstatus=="0"){
+  $searchStatus = '';
+}
+else {
+  $searchStatus = " AND assetStatus='$assetstatus'";
+}
+if($assetType=="0"){
+  $searchAssetType = '';
+}
+else {
+  $searchAssetType = " AND assetType='$assetType'";
+}
+if($assetVendor=="0"){
+  $searchAssetVendor = '';
+}
+else {
+  $searchAssetVendor = " AND assetVendor='$assetVendor'";
+}
+if($startDate==0){
+  $searchStartDate = '';
+
+}
+else {
+  $searchStartDate = " AND (STR_TO_DATE(assetDATE,'%Y-%m-%d')
+  BETWEEN STR_TO_DATE('$startDate', '%Y-%m-%d')
+  AND STR_TO_DATE('$enddate', '%Y-%m-%d'))";
+}
+
+//echo "searchStatus $searchStatus<br >";
+
+
+$showColum = implode(",", $_POST['showColum']);
+$columeArray = (explode(",",$showColum));
+$columeLength = count($columeArray);
+/* echo "columeArray $columeArray";
  echo " startDate $startDate <br >";
  echo " enddate $enddate <br >";
+ echo " assetVendor $assetVendor <br >";*/
 
-
+/*foreach ($columeArray as $key => $value) {
+  echo "$value";
+}*/
  $time = strtotime($startDate);
  $myFormatForView = date("Y-m-d", $time);
  //echo "$myFormatForView";
 
+$sql = "SELECT $showColum
+FROM  asset_table,vendor_table,assettype_table,status_table
+WHERE assetVendor=vendorID
+AND assetType=IDType
+AND assetStatus=statusID
+$searchStatus
+$searchAssetType
+$searchAssetVendor
+$searchStartDate ";
 
-$sql = "SELECT * from asset_table
-        where STR_TO_DATE(assetDATE, '%Y-%m-%d')
-        BETWEEN STR_TO_DATE('$startDate', '%Y-%m-%d')
-        AND STR_TO_DATE('$enddate', '%Y-%m-%d')";
+$rs =mysqli_query($link, $sql);
 
- $rs =mysqli_query($link, $sql);
+echo "<table class=\"table table-bordered\" id=\"colshow\">";
+echo "<caption\>รายงาน</caption>";
+echo "<thead>";
+echo "<tr>";
+foreach ($columeArray as $key => $value) {
+  echo "<th>$value </th>";
+}
+echo "</tr>";
+echo "</thead>";
+echo "<tbody>";
 
+$text ='';
  while($data = mysqli_fetch_array($rs)){
-   $time = strtotime($data['assetDate']);
-   $myFormatForView = date("Y-m-d", $time);
-   echo "ID {$data['assetID']}  assetDATE $myFormatForView<br >";
- }
+$text ='';
+echo "<tr>";
+   foreach ($columeArray as $key => $value) {
+     if ($value=='assetDate') {
+       $time = strtotime($data['assetDate']);
+       $myFormatForView = date("Y-m-d", $time);
+       echo "<td>";
+       echo "$myFormatForView";
+       echo "</td>";
+     }else {
+        $temp = $data[$value];
+        echo "<td>";
+        echo "$temp";
+        echo "</td>";
+      // $text +="<td>"+{$data[$value]}+"</td>";
+
+     }
+
+   }
+   echo "$text";
+  echo "</tr>";
+}
+
+echo "</tbody>";
+echo "</table>";
+
+
+
+
+
+
+  // echo "ID {$data['assetID']}  assetType {$data['assetType']}  assetVendor {$data['vendorName']} assetDATE $myFormatForView<br >";
+
 
 
 
--- view-report.php
+++ view-report.php
@@ -6,39 +6,111 @@
     background-color: #ffebcd;
   }
 </style>
-<script type="text/javascript">
-function  testCheck()
-{
-  var check = document.getElemenybyID.('timeCheck').checked;
-  if (check)
-  {
-    echo ("check");
-  }
 
-}
-</script>
 <div class="container-fluid">
   <div class="row">
     <div class="col-md-12">
-      <form action= "sql-report.php" method = "post" >
+      <form  action="sql-report.php" method = "POST">
         <div class="panel panel-danger">
           <div class="panel-heading">
             <h3 class="panel-title">Report</h3>
           </div>
-          <div class="panel-body">
+          <div  class="panel-body">
             <div class="row">
-              <div id="alldiv" class="col-md-4">
-                <div class="form-group" >
+              <div id="reportdiv" class="col-md-4">
+                <div class="form-group">
                   <label class="radio-inline">
-                    <input type="radio" name="report"  onchange="selectReport();" value="1"> รายงานอุปกรณ์
+                    <input type="radio" name="report"   value="1" > รายงานอุปกรณ์
                   </label>
                   <label class="radio-inline">
-                    <input type="radio" name="report" value="2"> รายงานการแจ้งซ่อม
+                    <input type="radio" id="maintainReport" name="report" value="2"> รายงานการแจ้งซ่อม
                   </label>
+                  <br>
+                      <br>
+                   <label for="">เลือกข้อมูล</label>
+                   <br>
+                     <input type="checkbox" name = "showColum[]" value ="assetID" > ID </input>
+                     <input type="checkbox" name = "showColum[]" value="assetName" > assetName </input>
+                     <input type="checkbox" name = "showColum[]" value="assetDate" > assetDate </input>
+                     <input type="checkbox" name = "showColum[]" value="assetExpire" > assetExpire </input>
+                         <br>
+                     <input type="checkbox" name = "showColum[]" value="assetPrice" > assetPrice </input>
+                     <input type="checkbox" name = "showColum[]" value="vendorName" > vendorName </input>
+                     <input type="checkbox" name = "showColum[]" value="typeName" > typeName </input>
+                      <input type="checkbox" name = "showColum[]" value="statusName" > statusName </input>
+
+                  <br>
+                  <br>
+
+                  <label>
+                    <input type="checkbox" name="selectDate" id="timeCheckbox"> เลือกช่วงเวลา
+                  </label>
+                  <div class="input-group">
+                    <input  id = "Date" type="date" name="assetStartDate" class="form-control"
+                  >
+                    <span class="input-group-addon"> To </span>
+                    <input  id = "endDate" type="date" name="assetEndDate" class="form-control"
+                    value="<?php
+                    $time = strtotime("2017-10-5");
+                   $myFormatForView = date("Y-m-d", $time);
+                   echo "$myFormatForView";
+                      ?>">
+                  </div>
+                  <br>
+                  <label>
+                    <input type="checkbox" name="selectStatus"> เลือกจากสถานะอุปกรณ์
+                  </label>
+                  <select class="form-control" name="assetStatus">
+                    <option value="0">เลือก</option>
+                    <?php include('sql-asset-status.php'); ?>
+                  </select>
+                  <br>
+                  <label>
+                    <input type="checkbox" name="selectType"> เลือกจากชนิดอุปกรณ์
+                  </label>
+                  <select class="form-control" name="assetType">
+                        <option value="0">เลือก</option>
+                    <?php include('sql-asset-type.php') ?>
+                  </select>
+                  <br>
+
+
+
+                  <label>
+                    <input type="checkbox" name="selectVendor"> เลือกจากผู้ผลิต
+                  </label>
+                  <select class="form-control" name="assetVendor">
+                    <option value="0">
+                      เลือก
+                    </option>
+                    <?php include('sql-asset-vendor.php') ?>
+                  </select>
+                  <br>
+
+
+
+              <body onload="maintainDivhidden()">
+
+
+                </div> <!-- form-group -->
+                <button type="submit" name="button"> summit</button>
+              </div> <!-- col-md-4 -->
+
+              <div id="maintainDiv"   >
+              <div class="col-md-4">
+                <div class="form-group">
+                  <label class="radio-inline">
+                    <input type="radio" id="assetReport" name="report" value="1"> รายงานอุปกรณ์
+                  </label>
+                  <label class="radio-inline">
+                    <input type="radio"  name="report" value="2" > รายงานการแจ้งซ่อม
+                  </label>
+                  <br>
                   <br>
                   <label>
                     <input type="checkbox" name="selectDate"   id="timeCheckbox" onclick="timeCheck()"> เลือกช่วงเวลา
                   </label>
+                  <br>
                   <div class="input-group">
                     <input  id = "Date" type="date" name="startDate" class="form-control" disabled >
                     <span class="input-group-addon"> To </span>
@@ -46,22 +118,18 @@ function  testCheck()
                   </div>
                   <br>
                   <label>
-                    <input type="checkbox" name="selectStatus"> เลือกจากสถานะ
+                    <input type="checkbox" name="selectStatus"> เลือกจากชนิดงานซ่อม
                   </label>
                   <select class="form-control" name="status">
                     <option value="0">เลือก</option>
-                    <?php include('asset-sql-status.php'); ?>
+                    <?php
+
+                    ?>
                   </select>
                   <br>
+
                   <label>
-                    <input type="checkbox" name="selectType"> เลือกจากชนิดอุปกรณ์
-                  </label>
-                  <select class="form-control" name="type">
-                    <?php include('asset-sql-type.php') ?>
-                  </select>
-                  <br>
-                  <label>
-                    <input type="checkbox" name="selectMaintain"> เลือกจากงาน
+                    <input type="checkbox" name="selectMaintain"> เลือกสถานะงานซ่อม
                   </label>
                   <select class="form-control" name="maintain">
                     <option value="0">
@@ -75,18 +143,10 @@ function  testCheck()
                     </option>
                   </select>
                   <br>
+
+
                   <label>
-                    <input type="checkbox" name="selectVendor"> เลือกจากผู้ผลิต
-                  </label>
-                  <select class="form-control" name="vendor">
-                    <option value="0">
-                      เลือก
-                    </option>
-                    <?php include('asset-sql-vendor.php') ?>
-                  </select>
-                  <br>
-                  <label>
-                    <input type="checkbox" name="selectUser"> เลือกจากช่าง
+                    <input type="checkbox" name="selectUser"> เลือกจากผู้ใช้
                   </label>
                   <select class="form-control" name="user">
                     <option value="0">
@@ -99,10 +159,13 @@ function  testCheck()
                       ช่าง2
                     </option>
                   </select>
-                  <button type="summit" name="button"> summit</button>
-                </div> <!-- form-group -->
 
+
+
+                </div> <!-- form-group -->
+                <button type="button" name="button"> submit</button>
               </div> <!-- col-md-4 -->
+           </div>
 
 
               <div class="col-md-8">
@@ -153,6 +216,24 @@ function  testCheck()
   </div>
 </div>
 <script type="text/javascript">
+$(document).ready(function(){
+    $("#maintainReport").change(function(){
+      $("#reportdiv").hide();
+      $("#maintainDiv").show();
+
+
+      });
+      $("#assetReport").change(function(){
+        $("#reportdiv").show();
+        $("#maintainDiv").hide();
+
+            });
+    });
+
+function maintainDivhidden(){
+  $("#maintainDiv").hide();
+}
+
   function timeCheck(){
     var x = document.getElementById("timeCheckbox").checked;
  if(x){
@@ -165,8 +246,5 @@ function  testCheck()
    }
 
    };
-   $("#hide").click(function(){
-    $("alldiv").hide();
-});
 
 </script>
